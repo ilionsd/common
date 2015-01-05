3aa9487837a182667c68d55566c7714561c7fa20
ilion <ilionsd@gmail.com>
2015-01-05 15:34:06 +0300

    get method implemented

--- src/com/insign/common/linearalgebra/Matrix.java
+++ src/com/insign/common/linearalgebra/Matrix.java
@@ -4,5 +4,14 @@ package com.insign.common.linearalgebra;
  * Created by ilion on 05.01.2015.
  */
 public class Matrix {
+    double[] array;
+    int rowsCount;
+    int columnsCount;
 
+    public double get(int row, int column) {
+        if (row < rowsCount && column < columnsCount)
+            return array[columnsCount * row + column];
+        else
+            throw new IndexException();
+    }
 }
