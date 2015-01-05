34dd01d0bedbbf15244fa89c365e19a65171a420
ilion <ilionsd@gmail.com>
2015-01-05 15:24:13 +0300

    created class

--- src/com/insign/common/Main.java
+++ src/com/insign/common/Main.java
@@ -4,4 +4,7 @@ package com.insign.common;
  * Created by ilion on 05.01.2015.
  */
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
--- src/com/insign/common/linearalgebra/Matrix.java
+++ src/com/insign/common/linearalgebra/Matrix.java
@@ -4,4 +4,5 @@ package com.insign.common.linearalgebra;
  * Created by ilion on 05.01.2015.
  */
 public class Matrix {
+
 }
