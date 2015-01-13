3ac744dd7c43f4ff880c7f105851c1835015e22a
Semyon Burov <ilionsd@gmail.com>
2015-01-13 18:37:55 +0300

    Added elementary transformations for matrix, scalar product and norm for vectors

--- src/com/insign/common/linearalgebra/LinearObjects/Vector.java
+++ src/com/insign/common/linearalgebra/LinearObjects/Vector.java
@@ -1,5 +1,7 @@
 package com.insign.common.linearalgebra.LinearObjects;
 
+import java.util.Objects;
+
 /**
  * Created by ilion on 06.01.2015.
  */
@@ -19,5 +21,26 @@ public interface Vector extends LinearObject {
 	Vector multiply(double multiplier);
 
 	@Override
-	Vector transpose();
+	void transpose();
+
+	public static class Math {
+		static double scalarProduct(Vector vector1, Vector vector2) {
+			Objects.requireNonNull(vector1, "Operand can not be null");
+			Objects.requireNonNull(vector2, "Operand can not be null");
+			if (vector1.getSize() != vector2.getSize())
+				throw new IllegalArgumentException("Vectors could not be different size");
+			double result = 0;
+			for (int k = 0; k < vector1.getSize(); k++)
+				result += vector1.get(k) * vector2.get(k);
+			return result;
+		}
+
+		static double norm(Vector vector) {
+			Objects.requireNonNull(vector, "Operand can not be null");
+			double result = 0;
+			for (int k = 0; k < vector.getSize(); k++)
+				result += vector.get(k) * vector.get(k);
+			return result;
+		}
+	}
 }
