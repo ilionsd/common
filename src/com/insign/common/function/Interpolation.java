8d237e6fd4267d9508e1601e5e18c101fb218a5e
Semyon Burov <ilionsd@gmail.com>
2015-02-10 11:46:59 +0300

    intexes and sizes of all matrix corrected

--- src/com/insign/common/function/Interpolation.java
+++ src/com/insign/common/function/Interpolation.java
@@ -17,11 +17,13 @@ public class Interpolation {
 	public static CubeSpline SmoothingSpline(Point2D[] points, double lambda) {
 		//-- x indexes changes from 0 to n  --
 		int n = points.length - 1;
+		//-- h indexes changes from 0 to n-1 --
 		int nh = n - 1;
 		double[] h = new double[nh + 1];
 		for (int k = 0; k <= nh; k++)
 			h[k] = points[k + 1].getX() - points[k].getX();
-		int nR = n - 1;
+		//-- R indexes changes from 0 to n-2 and from 0 to n-2 --
+		int nR = n - 2;
 		Matrix R = MatrixImpl.FACTORY.newInstance(nR + 1, nR + 1);
 		R.set(0, 0, 2 * (h[0] + h[1]));
 		R.set(0, 1, h[1]);
@@ -30,34 +32,47 @@ public class Interpolation {
 			R.set(i, i, 2 * (h[i] + h[i + 1]));
 			R.set(i, i + 1, h[i + 1]);
 		}
-		R.set(nR, nR - 1, h[nR - 1]);
-		R.set(nR, nR, 2 * (h[nR - 1] + h[nR]));
+		R.set(nR, nR - 1, h[nR]);
+		R.set(nR, nR, 2 * (h[nR] + h[nR + 1]));
 
-		int QtsizeRow = points.length - 1;
-		int QtsizeCol = points.length + 1;
-		Matrix Qt = MatrixImpl.FACTORY.newInstance(QtsizeRow, QtsizeCol);
-		for (int i = 0; i < QtsizeRow; i++) {
-			Qt.set(i, i, 3 / h[i]);
-			Qt.set(i, i + 1, -(3 / h[i] + 3 / h[i + 1]));
-			Qt.set(i, i + 2, 3 / h[i + 1]);
+		//-- Qt indexes changes from 0 to n-2 and from 0 to n --
+		int nQtRow = n - 2;
+		int nQtCol = n;
+		Matrix Qt = MatrixImpl.FACTORY.newInstance(nQtRow + 1, nQtCol + 1);
+		double rRight = 0;
+		double rLeft = 3.0 / h[0];
+		//-- last index = n-2 --
+		for (int i = 0; i <= nQtRow; i++) {
+			rRight = 3.0 / h[i + 1];
+			Qt.set(i, i, rLeft);
+			Qt.set(i, i + 1, -(rLeft + rRight));
+			Qt.set(i, i + 2, rRight);
+			rLeft = rRight;
 		}
 
 		Matrix Q = MatrixImpl.FACTORY.newInstance(Qt);
 		Q.transpose();
 
-		Matrix Sigma = MatrixImpl.FACTORY.E(points.length);
+		int nSigma = n;
+		Matrix Sigma = MatrixImpl.FACTORY.E(nSigma + 1);
 
-		Vector y = VectorImpl.FACTORY.newInstance(points.length);
+		Vector y = VectorImpl.FACTORY.newInstance(n + 1);
 		for (int k = 0; k < y.getSize(); k++)
 			y.set(k, points[k].getY());
 
-		double mu = 2.0 / 3.0 * (1 - lambda) / lambda;
+		double mu = (2.0 / 3.0) * (1 - lambda) / lambda;
 
 		Matrix A = Qt.multiply(Sigma).multiply(Q).multiply(mu).add(R);
 
 		Vector B = Qt.multiply(y);
 
 		Vector b = LAES.Gauss(A, B);
+
+		Vector bExt = VectorImpl.FACTORY.newInstance(b.getSize() + 2);
+		bExt.set(0, 0);
+		bExt.set(b.getSize() - 1, 0);
+		for (int k = 0; k < b.getSize(); k++)
+			bExt.set(k + 1, b.get(k));
 
 		Vector d = y.add(Sigma.multiply(Q).multiply(b).negate());
 
