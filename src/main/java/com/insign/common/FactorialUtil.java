84bbaadb010b955eef7c9af852d385679d2cc3cf
Semyon Burov <ilionsd@gmail.com>
2015-03-19 11:19:38 +0300

    Something changed

--- src/main/java/com/insign/common/FactorialUtil.java
+++ src/main/java/com/insign/common/FactorialUtil.java
@@ -13,11 +13,11 @@ public class FactorialUtil {
 	 * @return falling sequential product
 	 */
 	public static long fallingFactorial(long x, long n) {
-		if (x < 0 || n <= 0)
+		if (x < 0 || n < 0)
 			throw new IllegalArgumentException("x should be more or equals than 0 and n should be more than 0");
 		if (x < n)
 			throw new IllegalArgumentException("x should be more than n");
-		if (x == 0 && n == 1)
+		if (x == 0 || n == 0)
 			return 1;
 		long fact = 1;
 		long xEnd = x - n + 1;
